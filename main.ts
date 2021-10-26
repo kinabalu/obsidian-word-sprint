8f6a83c1dd45bc5c8a71d63900b308ee048e3611
Andrew Lombardi <andrew@mysticcoders.com>
2021-10-25 17:37:46 -0700

    testing out how to do a pomodora timer

--- main.ts
+++ main.ts
@@ -1,5 +1,7 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
+import numeral from 'numeral'
+
 interface WordSprintSettings {
 	mySetting: string;
 }
@@ -14,7 +16,34 @@ export default class WordSprintPlugin extends Plugin {
 	async onload() {
 		await this.loadSettings();
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
+		this.addCommand({
+			id: 'start-25-minute-word-sprint',
+			name: 'Start 25 minute Word Sprint',
+			callback: () => {
+				let secondsTotal = 25 * 60
+				let statusBarItemEl = this.addStatusBarItem()
+
+				const now = Date.now()
+				statusBarItemEl.setText('Word Sprint- 25:00 left')
+
+				const sprintInterval = window.setInterval(() => {
+					const currentNow = Date.now()
+					const elapsedSeconds = Math.floor((currentNow - now) / 1000)
+
+					const secondsLeft = secondsTotal - elapsedSeconds
+					const minutes = Math.floor(secondsLeft / 60)
+					const seconds = Math.ceil(secondsLeft % 60)
+					statusBarItemEl.setText(`Word Sprint - ${numeral(minutes).format('00')}:${numeral(seconds).format('00')} left`)
+
+					if (secondsLeft <= 0) {
+						window.clearInterval(sprintInterval)
+						statusBarItemEl.setText('')
+						new Notice('Word Sprint Complete! Congratulations!')
+					}
+				}, 1000)
+				this.registerInterval(sprintInterval);
+			}
+		})
 		this.addSettingTab(new WordSprintSettingsTab(this.app, this));
 	}
 
@@ -45,17 +74,5 @@ class WordSprintSettingsTab extends PluginSettingTab {
 		containerEl.empty();
 
 		containerEl.createEl('h2', {text: 'Word Sprint Settings'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
 	}
 }
