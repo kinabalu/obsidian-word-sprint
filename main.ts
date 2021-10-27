6c53f13eba8f52b9485e53eec880ec9074bafc36
Andrew Lombardi <andrew@mysticcoders.com>
2021-10-26 23:04:11 -0700

    word counts and time since last word written enable green, yellow and red status

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { App, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {App, Notice, Plugin, PluginSettingTab, Setting, debounce, TFile, MarkdownView, Debouncer} from 'obsidian';
 
 import numeral from 'numeral'
 
@@ -14,6 +14,15 @@ export default class WordSprintPlugin extends Plugin {
 	settings: WordSprintSettings;
 	sprintInterval : number;
 	statusBarItemEl : HTMLElement;
+	sprintStarted : boolean = false;
+	debouncedUpdate: Debouncer<[contents: string, filepath: string]>;
+
+	lastWordTime : number = 0;
+	previousWordCount : number;
+	wordCount : number = 0;
+
+	yellowNoticeShown : boolean = false
+	redNoticeShown : boolean = false
 
 	secondsToMMSS(seconds : number) {
 		const minutes = Math.floor(seconds / 60)
@@ -24,6 +33,41 @@ export default class WordSprintPlugin extends Plugin {
 	async onload() {
 		await this.loadSettings();
 
+		this.registerEvent(
+			this.app.workspace.on('quick-preview', this.onQuickPreview.bind(this))
+		)
+		// this.registerInterval(
+		// 	window.setInterval(() => {
+		// 	}, 200)
+		// )
+
+		this.debouncedUpdate = debounce((contents: string, filepath: string) => {
+			this.lastWordTime = Date.now()
+			const curr = this.getWordCount(contents)
+			this.wordCount = curr
+			// updateWordCount(contents: string, filepath: string) {
+			// 	const curr = this.getWordCount(contents);
+			// 	if (this.settings.dayCounts.hasOwnProperty(this.today)) {
+			// 		if (this.settings.todaysWordCount.hasOwnProperty(filepath)) {//updating existing file
+			// 			this.settings.todaysWordCount[filepath].current = curr;
+			// 		} else {//created new file during session
+			// 			this.settings.todaysWordCount[filepath] = { initial: curr, current: curr };
+			// 		}
+			// 	} else {//new day, flush the cache
+			// 		this.settings.todaysWordCount = {};
+			// 		this.settings.todaysWordCount[filepath] = { initial: curr, current: curr };
+			// 	}
+			// 	this.updateCounts();
+			// }
+		}, 400, false)
+
+		this.addCommand({
+			id: 'debug-test',
+			name: 'Debug Test',
+			callback: () => {
+				console.log('hi mom')
+			}
+		})
 		this.addCommand({
 			id: 'start-word-sprint',
 			name: 'Start Word Sprint',
@@ -31,20 +75,41 @@ export default class WordSprintPlugin extends Plugin {
 				let secondsTotal = this.settings.sprintLength * 60
 				this.statusBarItemEl = this.addStatusBarItem()
 
-				const now = Date.now()
-				this.statusBarItemEl.setText(`Word Sprint - ${this.secondsToMMSS(secondsTotal)} left`)
+				let status = 'GREEN'
 
+				const now = Date.now()
+				this.lastWordTime = Date.now()
+
+				this.statusBarItemEl.setText(`Word Sprint - ${this.secondsToMMSS(secondsTotal)} left - ${this.wordCount} words written`)
+
+				this.sprintStarted = true
 				this.sprintInterval = window.setInterval(() => {
 					const currentNow = Date.now()
 					const elapsedSeconds = Math.floor((currentNow - now) / 1000)
 
 					const secondsLeft = secondsTotal - elapsedSeconds
-					this.statusBarItemEl.setText(`Word Sprint - ${this.secondsToMMSS(secondsLeft)} left`)
+
+
+					const secondsSinceLastWord = Math.floor(Date.now() - this.lastWordTime) / 1000
+					if (secondsSinceLastWord >= 10 && !this.yellowNoticeShown) {
+						new Notice("Keep on writing, this is a sprint!")
+						this.yellowNoticeShown = true
+						status = 'YELLOW'
+					} else if (secondsSinceLastWord >= 60 && !this.redNoticeShown) {
+						new Notice("Enjoy that break, get back to writing when you're back")
+						this.redNoticeShown = true
+						status = 'RED'
+					} else if(secondsSinceLastWord < 10) {
+						this.yellowNoticeShown = false
+						this.redNoticeShown = false
+						status = 'GREEN'
+					}
+					this.statusBarItemEl.setText(`Word Sprint - ${this.secondsToMMSS(secondsLeft)} left - ${this.wordCount} words written - ${status}`)
 
 					if (secondsLeft <= 0) {
 						window.clearInterval(this.sprintInterval)
 						this.statusBarItemEl.setText('')
-						new Notice('Word Sprint Complete! Congratulations!')
+						new Notice(`Word Sprint Complete! Congratulations! Total words written: ${this.wordCount}`)
 					}
 				}, 1000)
 				this.registerInterval(this.sprintInterval);
@@ -57,7 +122,8 @@ export default class WordSprintPlugin extends Plugin {
 			callback: () => {
 				if (this.sprintInterval) {
 					this.statusBarItemEl.setText('')
-					new Notice('Word Sprint Cancelled!')
+					this.sprintStarted = false
+					new Notice(`Word Sprint Cancelled! Total words written: ${this.wordCount}`)
 					window.clearInterval(this.sprintInterval)
 				} else {
 					new Notice('No Word Sprint running')
@@ -67,8 +133,37 @@ export default class WordSprintPlugin extends Plugin {
 		this.addSettingTab(new WordSprintSettingsTab(this.app, this));
 	}
 
+	onQuickPreview(file: TFile, contents: string) {
+		if (this.app.workspace.getActiveViewOfType(MarkdownView)) {
+			if (this.sprintStarted) {
+				this.debouncedUpdate(contents, file.path)
+			}
+		}
+	}
+
 	onunload() {
 
+	}
+
+	//Credit: better-word-count by Luke Leppan (https://github.com/lukeleppan/better-word-count)
+	getWordCount(text: string) {
+		let words: number = 0;
+
+		const matches = text.match(
+			/[a-zA-Z0-9_\u0392-\u03c9\u00c0-\u00ff\u0600-\u06ff]+|[\u4e00-\u9fff\u3400-\u4dbf\uf900-\ufaff\u3040-\u309f\uac00-\ud7af]+/gm
+		);
+
+		if (matches) {
+			for (let i = 0; i < matches.length; i++) {
+				if (matches[i].charCodeAt(0) > 19968) {
+					words += matches[i].length;
+				} else {
+					words += 1;
+				}
+			}
+		}
+
+		return words;
 	}
 
 	async loadSettings() {
