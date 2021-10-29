c56d8a43e251572e36e7c8650a8eda5c7ebccba3
Andrew Lombardi <alombardi@twitter.com>
2021-10-28 17:40:11 -0700

    remove commented out block

--- main.ts
+++ main.ts
@@ -78,46 +78,6 @@ export default class WordSprintPlugin extends Plugin {
 			name: 'Start Word Sprint',
 			callback: () => {
 				this.startSprintCommand()
-
-				//
-				// if (this.theSprint && this.theSprint.isStarted()) {
-				// 	new Notice("Sprint already started! Please stop current sprint if you'd like to reset")
-				// 	return
-				// }
-				//
-				// let previousWordCount = 0
-				// if (this.app.workspace.getActiveViewOfType(MarkdownView)) {
-				// 	let viewData = this.app.workspace.getActiveViewOfType(MarkdownView).getViewData()
-				// 	previousWordCount = getWordCount(viewData)
-				// }
-				// this.statusBarItemEl = this.addStatusBarItem()
-				// this.debouncedUpdate = debounce((contents: string, filepath: string) => {
-				// 	this.theSprint.typingUpdate(contents, filepath)
-				// }, 400, false)
-				//
-				// const sprintInterval = this.theSprint.startSprint(previousWordCount, (status: string, statusChanged : boolean) => {
-				// 	const miniStats = this.theSprint.getMiniStats()
-				//
-				// 	if (statusChanged) {
-				// 		switch(status) {
-				// 			case 'YELLOW':
-				// 				this.settings.showLagNotices && new Notice(this.settings.yellowNoticeText)
-				// 				break
-				// 			case 'RED':
-				// 				this.settings.showLagNotices && new Notice(this.settings.redNoticeText)
-				// 				break
-				// 			default:
-				// 		}
-				// 	}
-				// 	this.statusBarItemEl.setText(`Word Sprint - ${miniStats.secondsLeft} left - ${miniStats.wordCount} words written`)
-				// },(sprintRunStat : SprintRunStat) => {
-				// 	window.clearInterval(this.sprintInterval)
-				// 	this.statusBarItemEl.setText('')
-				// 	new Notice(`Word Sprint Complete! Congratulations! Total words written: ${sprintRunStat.totalWordsWritten}`)
-				// 	new EndOfSprintStatsModal(this.app, sprintRunStat).open()
-				// })
-				//
-				// this.registerInterval(sprintInterval);
 			}
 		})
 
