2f6357408ca04a285f7fdbf5f1908d63434cc02e
Andrew Lombardi <alombardi@twitter.com>
2021-11-04 09:03:19 -0700

    now history of sprints available

--- src/EndOfSprintStatsModal.ts
+++ src/EndOfSprintStatsModal.ts
@@ -38,13 +38,14 @@ export default class EndOfSprintStatsModal extends Modal {
 			sprintLengthText = `${secondsToHumanize(this.sprintRunStat.sprintLength * 60)}\n`
 		}
 
-		new Setting(contentEl)
-			.setName("Sprint Date")
-			.addText((text) => {
-				text.setValue(moment(this.sprintRunStat.created).format('YYYY-MM-DD HH:mm:ss'))
-				text.setDisabled(true)
-			})
-
+		if (this.sprintHistory && this.sprintHistory.length > 1) {
+			new Setting(contentEl)
+				.setName("Sprint Date")
+				.addText((text) => {
+					text.setValue(moment(this.sprintRunStat.created).format('YYYY-MM-DD HH:mm:ss'))
+					text.setDisabled(true)
+				})
+		}
 		new Setting(contentEl)
 			.setName("Sprint Length")
 			.addText((text) => {
